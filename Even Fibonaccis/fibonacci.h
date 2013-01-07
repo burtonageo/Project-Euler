e1796f50619fe6bfb4cadf1d969b663d770f2205
George Burton <burtonageo@gmail.com>
2013-01-07 15:40:20 +0000

    Removed unneeded folders

--- Even Fibonaccis/fibonacci.h
+++
@@ -1,6 +0,0 @@
-#ifndef FIBONACCI_H
-#define FIBONACCI_H
-
-int fibonacci(int num);
-
-#endif // FIBONACCI_H
--- Largest Prime Factor/largestprimefactor.h
+++
@@ -1,6 +0,0 @@
-#ifndef LARGESTPRIMEFACTOR_H
-#define LARGESTPRIMEFACTOR_H
-
-int quadratic_sieve(long num);
-
-#endif // LARGESTPRIMEFACTOR_H
