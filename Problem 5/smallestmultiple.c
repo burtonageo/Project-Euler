96e77006d31f400ef7765fcfcdc5b7487314d9d7
George Burton <burtonageo@gmail.com>
2013-08-20 14:54:32 +0100

    Solved problem 5

--- Problem 5/smallestmultiple.c
+++ Problem 5/smallestmultiple.c
@@ -1,5 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+
 #include "smallestmultiple.h"
 
 int main(int argc, char **argv) {
+	const int divnum = 20;
+	int num = 2520;
+	bool solution_found = false;
+	
+	while (!solution_found) {
+		solution_found = num_is_evenly_divisible_by_all_nums_to(num, divnum);
+		if (!solution_found) {
+			num += 10;
+		}
+	}
+	
+	printf("%d\n", num); 
 	return 0;
+}
+
+bool num_is_evenly_divisible_by_all_nums_to(int num , int end) {
+	for (int i = 1; i < end; i++) {
+		if (num % i != 0) {
+			return false;
+		}
+	}
+	return true;
 }
--- Problem 5/smallestmultiple.h
+++ Problem 5/smallestmultiple.h
@@ -1,5 +1,8 @@
 #ifndef SMALLEST_MULTIPLE_H
 #define SMALLEST_MULTIPLE_H
 
+#include "stdbool.h"
+
+bool num_is_evenly_divisible_by_all_nums_to(int, int);
 
 #endif // SMALLEST_MUTLIPLE_H
